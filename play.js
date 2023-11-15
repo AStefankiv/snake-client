37fb066138522caab0f29cf765c31710509d7440
AStefankiv <stefankif35@gmail.com>
2023-11-14 20:50:15 -0700

    Create input.js

--- play.js
+++ play.js
@@ -1,6 +1,10 @@
 const net = require("net");
 const connect = require("./client");// establishes a connection with the game server
-
+// const stdin = require("process");
+const setupInput = require("./input");
 
 console.log("Connecting ...");
 connect();
+
+
+setupInput();
